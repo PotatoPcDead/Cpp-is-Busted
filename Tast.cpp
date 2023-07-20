f4c5ae092a8e3cc98fd0ed7647a9a971d7e32fe3
PotatoPcDead <potatopcdead@gmail.com>
2023-07-20 13:20:35 +0500

    helloo

--- Tast.cpp
+++ Tast.cpp
@@ -5,7 +5,8 @@ int main(){
     int Operator;
     int Num1;
     int Num2;
-    
-    std::cout << x;
+    std::cout << "Please enter any operator =   ";
+    std::cin >> Operator;
+    std::cout << Operator;
     return 0;
 }
