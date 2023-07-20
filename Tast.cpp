e9cb008906119343201a793ecc075d11e0f167c3
PotatoPcDead <potatopcdead@gmail.com>
2023-07-20 13:33:09 +0500

    Calculator made

--- Tast.cpp
+++ Tast.cpp
@@ -8,7 +8,7 @@ int main(){
     int Result = 0;
     std::cout << "Please enter any operator =   ";
     std::cin >> Operator;
-    std::cout << "Number 1=   ";
+    std::cout << "Number 1 =   ";
     std::cin >> Num1;
     std::cout << "Number 2 =   ";
     std::cin >> Num2;
@@ -17,7 +17,7 @@ int main(){
     case '*':
         Result = Num1 * Num2;
         break;
-        
+
     case '/':
         Result = Num1 / Num2;
         break;
@@ -26,12 +26,14 @@ int main(){
         Result = Num1 + Num2;
         break;
 
-    case "-":
+    case '-':
         Result = Num1- Num2;
         break;
 
     default:
+        std::cout << "Please enter an operator      ";
         break;
     }
+    std::cout << "Result = " << Result;
     return 0;
 }
