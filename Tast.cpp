c8bf815dad3a773bdeb177098544f566f6a4408b
PotatoPcDead <potatopcdead@gmail.com>
2023-07-20 13:28:56 +0500

    asd

--- Tast.cpp
+++ Tast.cpp
@@ -2,11 +2,25 @@
 
 
 int main(){
-    int Operator;
+    char Operator;
     int Num1;
     int Num2;
+    int Result = 0;
     std::cout << "Please enter any operator =   ";
     std::cin >> Operator;
-    std::cout << Operator;
+    std::cout << "Number 1=   ";
+    std::cin >> Num1;
+    std::cout << "Number 2 =   ";
+    std::cin >> Num2;
+    switch (Operator)
+    {
+    case '*':
+        Result = Num1 * Num2;
+    
+    case '/':
+        Result = Num1 / Num2;
+    default:
+        break;
+    }
     return 0;
 }
